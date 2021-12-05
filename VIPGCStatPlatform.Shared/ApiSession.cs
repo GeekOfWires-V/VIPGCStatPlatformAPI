b0d9b736c8f5248d282fa3a4e1616acaaffc6afd
GeekOfWires-V <geekofwires@vivaldi.net>
2021-12-05 02:39:54 -0500

    Update to add shared library between web assembly client project and web API project

---
+++ VIPGCStatPlatform.Shared/ApiSession.cs
@@ -0,0 +1,13 @@
+﻿namespace VIPGCStatPlatform.Shared;
+
+/// <summary>
+/// Ultimately serves to produce an "API Session"
+/// representation with a given client but does not serve a
+/// purpose similar to what Blazor Server clients do but
+/// rather a way to track a given session for a given
+/// user account login or other purpose
+/// </summary>
+public class ApiSession
+{
+    
+}
